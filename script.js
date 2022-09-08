0116b4be4af105818cbd4aef752b79ac73e394ee
ayush-ahire <ayushgithub8@gmail.com>
2022-09-08 20:12:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+let turn = "X";
+let gamewinner = false;
+// function to change the turn
+let changeturn = () => {
+  return turn === "X" ? "0" : "X";
+};
+// win logic
+const gamewin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
+  let wins = [
+    [0, 1, 2, 5, 5, 0],
+    [3, 4, 5, 5, 15, 0],
+    [6, 7, 8, 5, 25, 0],
+    [0, 3, 6, -5, 15, 90],
+    [1, 4, 7, 5, 15, 90],
+    [2, 5, 8, 15, 15, 90],
+    [0, 4, 8, 5, 15, 45],
+    [2, 4, 6, 5, 15, 135],
+  ];
+  wins.forEach((e) => {
+    if (
+      boxtext[e[0]].innerHTML === boxtext[e[1]].innerHTML &&
+      boxtext[e[2]].innerHTML === boxtext[e[1]].innerHTML &&
+      boxtext[e[0]].innerHTML !== ""
+    ) {
+      document.querySelector(".winnerinfo").innerText = `${
+        boxtext[e[0]].innerText
+      } is the winner!!!`;
+      gamewinner = true;
+      document.querySelector(".line").style.width = "20vw";
+      document.querySelector(
+        ".line"
+      ).style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+    }
+  });
+};
+//game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turn = changeturn();
+      gamewin();
+      if (!gamewinner) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn of " + turn;
+      }
+    }
+  });
+});
+
+// adding eventlistner to reset btn
+reset.addEventListener("click", () => {
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  turn = "X";
+  gamewinner = false;
+  document.querySelector(".line").style.width = "0vw";
+  document.getElementsByClassName("info")[0].innerText = "Turn of " + turn;
+  document.querySelector(".winnerinfo").innerText = "";
+});
